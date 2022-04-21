846442f9ac1f4dc0aec6e5739b105524e5c33d8f
Nicholas Wierenga <NicholasWierenga@gmail.com>
2022-04-21 09:31:53 -0400

    initial commit for testBranch

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -4,19 +4,17 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
-        Console.Write("Please enter length: ");
-        decimal length = decimal.Parse(Console.ReadLine());
-        Console.Write("Please enter width: ");
-        decimal width = decimal.Parse(Console.ReadLine());
-        Console.Write("Please enter height: ");
-        decimal height = decimal.Parse(Console.ReadLine()); 
+
+        decimal length = getValidDec("Please enter length: ");
+        decimal width = getValidDec("Please enter width: ");
+        decimal height = getValidDec("Please enter height: ");
 
 
-        Console.WriteLine("Area: " + (length * width));
-        Console.WriteLine("Perimeter: " + (2 * (length + width)));
-        Console.WriteLine("Volume: " + (length * width * height));
-        Console.WriteLine("Surface area: " + (2 * ((length * width) + (length * height) + (width * height))));
-        switch (length * width)
+        Console.WriteLine("Area: " + getArea(length, width));
+        Console.WriteLine("Perimeter: " + getPerimeter(length, width));
+        Console.WriteLine("Volume: " + getVolume(length, width, height));
+        Console.WriteLine("Surface area: " + getSurfaceArea(length, width, height));
+        switch (getArea(length, width))
         {
             case <= 250:
                 Console.WriteLine("This is a small-sized room.");
@@ -30,5 +28,46 @@ public class Program
 
         }
         Console.WriteLine("Thank you for using the Room Detail Generator!");
+    }
+
+    public static decimal getValidDec(string prompt)
+    {
+        decimal dec = -1;
+
+        Console.WriteLine(prompt);
+
+        try
+        { 
+            dec = decimal.Parse(Console.ReadLine());
+            if (dec < 0)
+            {
+                Console.WriteLine("This can't be negative.");
+                getValidDec(prompt);
+            }
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("This is not a valid decimal number.");
+            getValidDec(prompt);
+        }
+
+        return dec;
+    }
+
+    public static decimal getArea(decimal length, decimal width)
+    {
+        return length * width;
+    }
+    public static decimal getPerimeter(decimal length, decimal width)
+    {
+        return 2 * (length + width);
+    }
+    public static decimal getVolume(decimal length, decimal width, decimal height)
+    {
+        return length * width * height;
+    }
+    public static decimal getSurfaceArea(decimal length, decimal width, decimal height)
+    {
+        return 2 * ((length * width) + (length * height) + (width * height));
     }
 }
